97121cbca5ac71fb085be8908c7aff1159546e0d
mattkram <mkramer@anaconda.com>
2024-08-16 21:59:36 -0500

    Use explicit imports

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-#[macro_use] extern crate rocket;
+use rocket::{get, launch, routes};
+// Alternate which will expose all macros globally
+// See note in: https://rocket.rs/guide/v0.5/overview/#routing
+// #[macro_use] extern crate rocket;
 
 #[get("/")]
 fn index() -> &'static str {
