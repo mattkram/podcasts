06c473507642a44c8e0745f6954a2f273c1a7ccc
mattkram <mkramer@anaconda.com>
2024-08-16 22:10:20 -0500

    Add parametrized route and static file hosting

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,21 @@ use rocket::{get, launch, routes};
 // Alternate which will expose all macros globally
 // See note in: https://rocket.rs/guide/v0.5/overview/#routing
 // #[macro_use] extern crate rocket;
+use rocket::fs::FileServer;
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/hello/<name>")]
+fn hello(name: &str) -> String {
+    format!("Hello, {}!", name)
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .mount("/", routes![index, hello])
+        .mount("/static", FileServer::from("./static"))
 }
