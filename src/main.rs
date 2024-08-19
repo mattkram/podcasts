1db13053280c1e2267843506cce2f0e0f538c203
mattkram <mkramer@anaconda.com>
2024-08-18 22:06:05 -0500

    Add templating support

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 #[macro_use]
 extern crate rocket;
 
-use rocket::fs::{FileServer, NamedFile};
-use std::path::Path;
+use rocket::fs::FileServer;
+use rocket_dyn_templates::context;
+use rocket_dyn_templates::Template;
 
 #[get("/")]
-async fn index() -> Option<NamedFile> {
-    NamedFile::open(Path::new("templates/index.html"))
-        .await
-        .ok()
+fn index() -> Template {
+    Template::render("index", context! {})
 }
 
 #[get("/clicked?<name>")]
@@ -27,4 +26,5 @@ fn rocket() -> _ {
     rocket::build()
         .mount("/", routes![index, handle_click])
         .mount("/static", FileServer::from("./static"))
+        .attach(Template::fairing())
 }
