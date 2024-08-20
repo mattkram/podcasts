0f4e2c78ff9b2ff467e179314cfee908f21d43bc
mattkram <mkramer@anaconda.com>
2024-08-19 22:27:50 -0500

    Run database migrations via sqlx::migrate

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 #[macro_use]
 extern crate rocket;
 
+use rocket::fairing::{self, AdHoc};
 use rocket::fs::FileServer;
+use rocket::{Build, Rocket};
 use rocket_db_pools::sqlx::{self, Row};
 use rocket_db_pools::{Connection, Database};
 use rocket_dyn_templates::context;
@@ -18,13 +20,6 @@ fn index() -> Template {
 
 #[get("/<id>")]
 async fn read(mut db: Connection<Logs>, id: i64) -> Option<String> {
-    sqlx::query(
-        "CREATE TABLE IF NOT EXISTS logs (id INTEGER PRIMARY KEY AUTOINCREMENT, content TEXT);",
-    )
-    .execute(&mut **db)
-    .await
-    .ok();
-
     sqlx::query("SELECT content FROM logs WHERE id = ?")
         .bind(id)
         .fetch_one(&mut **db)
@@ -44,10 +39,24 @@ fn handle_click(name: Option<&str>) -> String {
     format!("Ouch, {}!", _name)
 }
 
+async fn run_migrations(rocket: Rocket<Build>) -> fairing::Result {
+    match Logs::fetch(&rocket) {
+        Some(db) => match sqlx::migrate!("./migrations").run(&**db).await {
+            Ok(_) => Ok(rocket),
+            Err(e) => {
+                error!("Failed to initialize SQLx database: {}", e);
+                Err(rocket)
+            }
+        },
+        None => Err(rocket),
+    }
+}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .attach(Logs::init())
+        .attach(AdHoc::try_on_ignite("Run SQLx Migrations", run_migrations))
         .mount("/", routes![index, handle_click, read])
         .mount("/static", FileServer::from("./static"))
         .attach(Template::fairing())
